c95a2c8e4253209c4cb17f49d30ac23116c21d85
Mubarak-Ansari <mubarakansari8948@gmail.com>
2025-09-27 22:37:49 +0530

    Lecture-10

---
+++ Lecture-10/String.java
@@ -0,0 +1,24 @@
+
+
+// Live class-18
+// Date - 27th Sept 2025
+// String Algorithms
+// 1. String Matching Algorithms
+// 2. Pattern Searching Algorithms
+
+/*
+ * Character Arrays
+ * we use '\0'(null terminator) to indicate end of character array
+ * example- char[] ch = {'a','b','c','d','\0'};
+ * we write string in single quotes
+ * String- sequence of characters
+ * if we write string in double quotes then no need to use null terminator
+ * example- String str = "abcd"; or "Sane"
+ * String is a class in java
+ * String is immutable- we cannot change string once created   
+ * 
+ * rule vs convention
+ * rule- must follow  -> aisa karna hi padega
+ * convention- should follow -> aisa karna chahiye, nhi karoge fir v chalega
+ * First letter of class name should be in uppercase- convention
+ */
